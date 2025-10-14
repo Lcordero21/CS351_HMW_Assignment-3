e8940fa289fc6a70881466466ae634655ca7210d
Lcordero21 <lcordero@willamette.edu>
2025-10-13 20:27:54 -0700

    lots of work

--- graph_impl.py
+++ graph_impl.py
@@ -1,64 +1,71 @@
 from graph_interfaces import IEdge, IGraph, IVertex
-from typing import List, Protocol
+from typing import Dict, List, Protocol
 
 # Implementation definitions
 # You should implement the bodies of the methods required by the interface protocols.
 
 class Graph(IGraph):
     def __init__(self):
-        self.main_graph = {}
+        self._adj_list: Dict[str, List[str]] = {}
+
     def get_vertices(self) -> List[IVertex]: 
-        return self.main_graph.keys()
+        pass
     def get_edges(self) -> List[IEdge]: 
-        #FINISH
-        for vertex in range(self.main_graph):
-            pass
         pass
     def add_vertex(self, vertex: IVertex) -> None: 
-        #DOUBLE CHECK!!
-        Vertex.set_name(vertex)
-        self.main_graph[vertex]= []
+        pass
     def remove_vertex(self, vertex_name: str) -> None: 
-        self.main_graph.pop(vertex_name)
+        pass
     def add_edge(self, vertex1, vertex2) -> None: 
-        Vertex.add_edge(self.main_graph,vertex1,vertex2)
+        pass
     def remove_edge(self, vertex1, vertex2) -> None: 
-        Vertex.remove_edge(self, vertex1, vertex2)
+        pass
 
 
 
 class Vertex(IVertex):
-    def _init_(self):
-        self._visited = False
-        self._name = None
+    def _init_(self, name):
+        self._visited: bool = False
+        self._name: str = name
+        self._edges: List[IEdge] = []
+
     def get_name(self) -> str: 
         return self._name
+    
     def set_name(self, name: str) -> None:
         self._name = name
-    def add_edge(self, graph, vertex1, vertex2) -> None: 
-        graph[vertex1].append(vertex2)
-        graph[vertex2].append(vertex1)
-    def remove_edge(self, graph, vertex1, vertex2) -> None:
-        graph[vertex1].remove(vertex2)
-        graph[vertex2].remove(vertex1)
+
+    def add_edge(self, edge) -> None: 
+        self._edges.append(edge)
+
+    def remove_edge(self, edge_name) -> None:
+        for edge in self._edges:
+            if edge.get_name() == edge_name:
+                self._edges.remove(edge)
+                return
+            
     def get_edges(self) -> List[IEdge]: 
-        pass
+        return self._edges
+
     def set_visited(self, visited: bool) -> None: 
-        self._visited = True
+        self._visited = visited
+
     def is_visited(self) -> bool: 
         return self._visited
 
 class Edge(IEdge):
-    def __init__(self):
-        self._name = None
-        self._weight = None
+    def __init__(self, name: str, destination) -> None:
+        self._name: str = name
+        self._destination = destination
+        self._is_bi_directional: bool = False
+        self._weight: float = None
     def get_name(self) -> str: 
-        pass
+        return self._name
     def set_name(self, name: str) -> None: 
-        pass
+        self._name = name
     def get_destination(self) -> IVertex: 
-        pass
+        return self._destination
     def get_weight(self) -> float: 
-        pass
+        return self._weight
     def set_weight(self, weight: float) -> None:
-        pass
+        self._weight = weight
